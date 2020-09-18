a323793e5071cd770d4f512b37fa9db15229d86d
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-09-18 04:37:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,37 +9,37 @@ class Student
     @grade = grade
   end
   
-  def self.create_table
-    sql = <<- SQL 
-      CREATE TABLE IF NOT EXISTS students (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        grade TEXT
-        )
-    SQL 
-    DB[:conn].execute(sql)
-  end 
+ # def self.create_table
+#    sql = <<- SQL 
+#      CREATE TABLE IF NOT EXISTS students (
+#        id INTEGER PRIMARY KEY,
+#        name TEXT,
+#        grade TEXT
+#        )
+#    SQL 
+#    DB[:conn].execute(sql)
+#  end 
   
-  def self.drop_table 
-    sql = <<- SQL
-      DROP TABLE students
-    SQL 
-    DB[:conn].execute(sql)
-  end
+#  def self.drop_table 
+#    sql = <<- SQL
+#      DROP TABLE students
+#    SQL 
+#    DB[:conn].execute(sql)
+#  end
   
-  def save(name, grade)
-    sql = <<- SQL
-      INSERT INTO students (name, grade)
-      VALUES ( ?, ? )
-    SQL 
-    DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
-  end 
+#  def save(name, grade)
+#    sql = <<- SQL
+#      INSERT INTO students (name, grade)
+#      VALUES ( ?, ? )
+#    SQL 
+#    DB[:conn].execute(sql, self.name, self.grade)
+#    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+#  end 
   
-  def self.create(name, grade) 
-    student = Student.new(name, grade)
-    student.save
-    student
-  end 
+#  def self.create(name, grade) 
+#    student = Student.new(name, grade)
+#    student.save
+#    student
+#  end 
   
 end
