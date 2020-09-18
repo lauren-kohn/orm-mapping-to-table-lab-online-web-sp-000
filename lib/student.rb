d44b1e5a15b71ae6b9c45b77d71e8b6ddfe654ac
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-09-18 04:47:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,15 +10,8 @@ class Student
   end
   
   def self.create_table
-    sql =  <<- SQL 
-      {CREATE TABLE IF NOT EXISTS students (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        grade TEXT
-        )
-        }
-    SQL 
-    #DB[:conn].execute(sql)
+    sql = "CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT, grade TEXT)"
+    DB[:conn].execute(sql)
   end 
   
 #  def self.drop_table 
