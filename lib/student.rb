7d0389753d981049d137cc732a5a72c127d2d791
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-09-18 05:06:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,7 +27,7 @@ class Student
   
   def self.create(hash) 
     #student = hash.each { |key, value| self.send(("#{key}="), value) }
-    student = Student.new(name:, grade:)
+    student = Student.new(name: , grade:)
     student.save
     student
   end 
